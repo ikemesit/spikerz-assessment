98f7e4e11bc4c69cd6e979aae4bce29e72130689
Ikemesit <ikemesit@gmail.com>
2025-10-21 15:15:30 +0100

    fix(routes): add catch-all wildcard route to fix reload break

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,9 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./views/home/home').then((m) => m.Home),
   },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
